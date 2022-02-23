ccbea610a4ca17ff3453fdc8c10587c4c63342f3
Andrew Bruce <me@andrewbruce.net>
2022-02-23 10:22:48 +0000

    Add jupiter and google dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,16 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.6.10"
 }
+
+
+
 dependencies {
+    implementation(platform("com.google.cloud:libraries-bom:24.3.0"))
+
     implementation(kotlin("stdlib-jdk8"))
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+    implementation("com.google.cloud:google-cloud-storage")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 }
 repositories {
     mavenCentral()
