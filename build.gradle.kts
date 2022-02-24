4cb755c103d7e1d0d17c8506c13b68eb82d96abb
Andrew Bruce <me@andrewbruce.net>
2022-02-24 17:30:41 +0000

    Target Java 11
    Latest version permitted in our base docker image

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,12 +22,12 @@ repositories {
 
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "11"
 }
 
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "11"
 }
 
 tasks.register("createOutScript", CreateStartScripts::class.java) {
